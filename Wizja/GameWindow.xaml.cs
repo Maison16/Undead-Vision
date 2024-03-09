a64b5bd60c10f7080889338375a2b8fcda30e147
w6rstian <quarennc@gmail.com>
2024-03-09 01:51:55 +0100

    Movement sketch

--- Wizja/GameWindow.xaml.cs
+++ Wizja/GameWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wizja.classes;
 
 namespace Wizja
 {
@@ -19,9 +20,42 @@ namespace Wizja
     /// </summary>
     public partial class GameWindow : Window
     {
+        int direction = 0x0000;
         public GameWindow()
         {
             InitializeComponent();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            int direction = 0x0000;
+
+            if (e.Key == Key.W)
+                direction += 0x1000;
+
+            if (e.Key == Key.A)
+                direction += 0x0100;
+
+            if (e.Key == Key.S)
+                direction += 0x0010;
+
+            if (e.Key == Key.D)
+                direction += 0x0001;
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+                direction -= 0x1000;
+
+            if (e.Key == Key.A)
+                direction -= 0x0100;
+
+            if (e.Key == Key.S)
+                direction -= 0x0010;
+
+            if (e.Key == Key.D)
+                direction -= 0x0001;
         }
     }
 }
