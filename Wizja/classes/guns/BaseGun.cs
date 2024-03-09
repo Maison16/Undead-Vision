0d3ad524f7b54115c99df5c67f6a6e965d410a17
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-09 23:35:24 +0100

    zmiana ground

--- Wizja/classes/guns/BaseGun.cs
+++ Wizja/classes/guns/BaseGun.cs
@@ -18,7 +18,7 @@ namespace Wizja.classes.guns
     public class BaseGun : Weapon
     {
         public static BitmapImage img = new BitmapImage(new Uri("pack://application:,,,/res/pistol.png"));
-        public BaseGun() : base("Pisol", 1, 250, 100, img) { }
+        public BaseGun() : base("Pistol", 1, 250, 100, img) { }
 
         public override void Shoot(Point playerPos, Vector direction, List<Rectangle> targets, List<Enemy> enemies, Canvas gameCanvas)
         {
