a1d1ac66afe72814a5e72edb16b70095c0e6f952
Kex <krzysieksobol9@gmail.com>
2024-03-09 03:03:18 +0100

    shooting beta version - almost done

--- Wizja/GameWindow.xaml.cs
+++ Wizja/GameWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wizja.classes;
 
 namespace Wizja
 {
@@ -19,9 +20,62 @@ namespace Wizja
     /// </summary>
     public partial class GameWindow : Window
     {
+        private Point playerPosition;
+        private Rectangle enemy;
         public GameWindow()
         {
             InitializeComponent();
+
+            // TESTY TESCIKI //
+            // test kloc jako player
+            Rectangle playerRect = new Rectangle
+            {
+                Width = 20,
+                Height = 20,
+                Fill = Brushes.Blue
+            };
+            Canvas.SetLeft(playerRect, 960);
+            Canvas.SetTop(playerRect, 512);
+            gameCanvas.Children.Add(playerRect);
+            playerPosition = new Point(Canvas.GetLeft(playerRect), Canvas.GetTop(playerRect));
+
+            PreviewKeyDown += GameWindow_PreviewKeyDown;
+
+
+            // test przeciwnik
+            enemy = new Rectangle
+            {
+                Width = 35,
+                Height = 35,
+                Fill = Brushes.Black
+            };
+            Canvas.SetLeft(enemy, 860);
+            Canvas.SetTop(enemy, 512);
+            gameCanvas.Children.Add(enemy);
+        }
+
+        private void GameWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                ShootProjectile();
+            }
+        }
+
+        private void ShootProjectile()
+        {
+            // pozycja kursora
+            Point cursorPosition = Mouse.GetPosition(gameCanvas);
+
+            // wektor pocisku
+            Vector direction = cursorPosition - playerPosition;
+            direction.Normalize();
+
+            // koncowy punkt 
+            Point endPoint = playerPosition + direction * 500; // 500 to range do zmiany 
+
+            // Stworz pocisk
+            Projectile projectile = new Projectile(playerPosition.X, playerPosition.Y, endPoint.X, endPoint.Y, 2, enemy, gameCanvas);
         }
     }
 }
